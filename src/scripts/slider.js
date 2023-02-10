074af486c34cda99d971cff34a42e3837d383ea4
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-02-11 01:09:38 +0530

    [RU_08]: Fix peek issue in carousel

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,19 +1,28 @@
-import Glide, {
-  Controls,
-  Breakpoints,
-  Swipe,
-  Autoplay,
-  Keyboard,
-} from "@glidejs/glide/dist/glide.modular.esm";
+import Glide from "@glidejs/glide";
 
-var carousel = new Glide(".glide", {
+const carousel = new Glide(".glide", {
   type: "carousel",
-  autoplay: 1000,
+  autoplay: 5000,
   keyboard: true,
-  animationDuration: 3000,
+  animationDuration: 1000,
   animationTimingFunc: "linear",
   touchRatio: 0.5,
-  perView: 1,
   gap: 30,
+
+  breakpoints: {
+    568: {
+      perView: 1,
+      peek: 0,
+    },
+    768: {
+      peek: 100,
+    },
+    992: {
+      peek: 120,
+    },
+    4000: {
+      peek: 200,
+    },
+  },
 });
-carousel.mount({ Controls, Breakpoints, Swipe, Autoplay, Keyboard });
+carousel.mount();
