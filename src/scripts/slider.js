03b50998e4a53b5524fca6cbf66b15ab0aa78de5
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-02-10 14:17:39 +0530

    [RU_08]: Add carousel

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,41 +1,19 @@
+import Glide, {
+  Controls,
+  Breakpoints,
+  Swipe,
+  Autoplay,
+  Keyboard,
+} from "@glidejs/glide/dist/glide.modular.esm";
 
-import Glide, { Controls, Breakpoints , Swipe, Autoplay , Keyboard} from '@glidejs/glide/dist/glide.modular.esm'
-
-const carousel = new Glide('.glide',{
-    type: 'carousel',
-    autoplay:3000,
-    keyboard:true,
-    animationDuration: 1000,
-    animationTimingFunc: 'linear',
-    touchRatio:0.5,
-    perView: 1,
-    gap:  30,
-    // peek: {
-    //           before: 50,
-    //           after: 50
-    //         }
-    
-
-    Breakpoints:{ 
-      568:{
-        perView:1,
-      },
-      768:{
-        perView:1,
-
-      },
-      992: {
-        gap:50,
-        startAt: 0,
-        perView: 1,
-        peek: {
-          before: 50,
-          after: 50
-        }
-      },
-    }
-    
-
+var carousel = new Glide(".glide", {
+  type: "carousel",
+  autoplay: 1000,
+  keyboard: true,
+  animationDuration: 3000,
+  animationTimingFunc: "linear",
+  touchRatio: 0.5,
+  perView: 1,
+  gap: 30,
 });
-carousel.mount({ Controls, Breakpoints , Swipe, Autoplay , Keyboard})
-
+carousel.mount({ Controls, Breakpoints, Swipe, Autoplay, Keyboard });
