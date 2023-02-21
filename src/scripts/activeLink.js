15a52c60295eb8d8ff3a612f4d5f859137454f6c
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-02-21 20:11:24 +0530

    handle active state on pg reload

--- src/scripts/activeLink.js
+++ src/scripts/activeLink.js
@@ -1,5 +1,16 @@
 const active = "active-link";
 
+/** //selects all anchor elements with a 'href' attribute that matches the current page's location hash,
+ * and add active class on each link found
+ * @function
+ * @param {string} hash - The current page's location hash
+ */
+const addActiveStateToLink = (hash) => {
+  document.querySelectorAll(hash).forEach((link) => {
+    link.classList.add(active);
+  });
+};
+
 /** Handles the active link in the navigation. */
 const handleActiveLink = () => {
   // Select the currently active links and remove the active class.
@@ -8,11 +19,13 @@ const handleActiveLink = () => {
     link.classList.remove(active);
   });
 
-  //selects all anchor elements with a 'href' attribute that matches the current page's location hash, and add active class on each link found
-  document.querySelectorAll(`a[href="${document.location.hash}"]`).forEach((link) => {
-    link.classList.add(active);
-  });
+  addActiveStateToLink(`a[href="${document.location.hash}"]`);
 };
 
 //used hashchange event to detect if URL has changed.
 window.addEventListener("hashchange", handleActiveLink);
+
+//used readystatechange event to keep the state of active link preserved on pg reload
+document.addEventListener("readystatechange", () => {
+  addActiveStateToLink(`a[href="${document.location.hash}"]`);
+});
